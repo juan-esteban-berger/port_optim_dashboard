3becb429df73f34a3979161e90f731c861995741
juan-esteban-berger <juanestebanberger@gmail.com>
2024-03-07 11:46:00 -0600

    V2 Dashboard

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,9 @@ import polars as pl
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+import altair
+from sklearn.preprocessing import StandardScaler
+
 
 import yfinance as yf
 
@@ -109,6 +112,27 @@ df_stock_returns = df_stock_returns.select([
 ])
 
 ###########################################################################
+# Metrics
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.metric(label="Number of Portfolios", value=df.shape[0])
+
+with col2:
+    st.metric(label="Number of Stocks", value=df_weights['Stock'].n_unique())
+
+df_stock_returns2 = df_stock_returns.to_pandas()
+df_stock_returns2['Date'] = pd.to_datetime(df_stock_returns2['Date'])
+earliest_date = df_stock_returns2['Date'].min().strftime('%Y-%m-%d')
+latest_date = df_stock_returns2['Date'].max().strftime('%Y-%m-%d')
+
+with col3:
+    st.metric(label="Earliest Date", value=earliest_date)
+
+with col4:
+    st.metric(label="Latest Date", value=latest_date)
+
+###########################################################################
 # Get the Stock Prices from Yahoo Finance
 earliest_date = df_stock_returns['Date'].min()
 latest_date = df_stock_returns['Date'].max()
@@ -122,8 +146,8 @@ prices = prices['Adj Close']
 
 ###########################################################################
 # Calculate the Efficient Frontier
-lowest_risk = df['Portfolio Risk'].min()
-highest_risk = df.filter(pl.col('Expected Return') > 0.7)['Portfolio Risk'].max()
+lowest_risk = 3.6
+highest_risk = 6.5
 
 risk_range = np.linspace(lowest_risk, highest_risk, 100)
 
@@ -139,9 +163,11 @@ frontier.columns = ['Portfolio Risk', 'Expected Return']
 
 ###########################################################################
 # Create Efficient Frontier Plot
+# Randomly select 10% of rows
+df_mini = df.sample(n=5000, seed=0)
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(x=df['Portfolio Risk'], y=df['Expected Return'],
+fig.add_trace(go.Scatter(x=df_mini['Portfolio Risk'], y=df_mini['Expected Return'],
                          mode='markers', name='Portfolios'))
 
 fig.add_trace(go.Scatter(x=frontier['Portfolio Risk'], y=frontier['Expected Return'],
@@ -152,7 +178,7 @@ fig.update_layout(xaxis_title='Portfolio Risk',
                   yaxis_title='Expected Return',
                   showlegend=True)
 
-risk_level = st.slider("Risk Level", min_value=1.65, max_value=3.7, value=2.30, step=0.01)
+risk_level = st.slider("Risk Level", min_value=3.8, max_value=6.5, value=4.2, step=0.01)
 
 fig.add_vline(x=risk_level, line_width=3, line_color="green")
 
@@ -174,9 +200,12 @@ df_filt = df_filt.join(df2, on='PortfolioID', how='left')
 col1, col2 = st.columns(2)
 
 with col1:
+    # Add a title to the plot
+    fig.update_layout(title='Efficient Frontier')
     st.plotly_chart(fig, use_container_width=True)
 
 with col2:
+    # Add a title to the plot
     fig = px.treemap(df_filt.to_pandas(), path=['Stock'], values='Weight')
     fig.update_traces(textinfo='label+percent entry')
     fig.update_traces(hovertemplate='Stock: %{label}<br>Weight: %{value:.2f}')
@@ -184,34 +213,100 @@ with col2:
     # Ensure that AAPL is colored blue
     # Ensure that NVDA is colored green
     # Ensure that CHPT is colored red
-    fig.update_traces(marker=dict(colors=['blue', 'red', 'lightcoral', 'green']))
+    fig.update_traces(marker=dict(colors=['blue',
+                                          'orange',
+                                          'red',
+                                          'violet',
+                                          'green',
+                                          'lightblue']))
+    fig.update_layout(title='Portfolio Weights')
     st.plotly_chart(fig, use_container_width=True)
 
 ###########################################################################
-# Plot the Prices using Plotly Express Line Charts
-col1, col2, col3, col4 = st.columns(4)
+# Define Color Dictionary
+# A dictionary to store line colors for different stocks for differentiation
+color_dict = {'AAPL':'blue', 'BB':'orange', 'GPRO':'red', 'MSFT':'violet','NVDA':'green', 'T':'lightblue'}
+
+###########################################################################
+col1, col2, col3 = st.columns(3)
+
+prices = prices.melt(ignore_index=False)
+
+stocks = prices['Ticker'].unique()
+
+# Multi Select Filter for 'Ticker'
+with col1:
+    tickers = st.multiselect('Select Ticker', stocks)
+
+# Start Date Filter
+with col2:
+    start_date = st.date_input('Start Date', earliest_date)
+
+# End Date Filter
+with col3:
+    end_date = st.date_input('End Date', latest_date)
+
+# Download the prices from yfinance
+prices_data = yf.download("AAPL BB GPRO MSFT NVDA T", start=start_date, end=end_date)
+# Only Select Adj Close
+prices_data = prices_data['Adj Close']
+
+temp_cols = prices_data.columns
+
+# Standardize the Data
+scaler = StandardScaler()
+prices_data = scaler.fit_transform(prices_data)
+
+prices_data = pd.DataFrame(prices_data, columns=temp_cols)
+
+# Apply Filters
+if tickers != []:
+    # Filter the Data
+    prices_data = prices_data[tickers]
+    df_stock_returns = df_stock_returns.filter(pl.col('Stock').is_in(tickers))
+
+# Plot the Prices
+fig = go.Figure()
+
+color_dict = {'AAPL':'blue', 'BB':'orange', 'GPRO':'red', 'MSFT':'violet','NVDA':'green', 'T':'lightblue'}
+fig = px.line(prices_data,
+              x=prices_data.index,
+              y=prices_data.columns,
+              color_discrete_map=color_dict)
+
+fig.update_layout(xaxis_title='Date',
+                    yaxis_title='Price',
+                    showlegend=True)
+
+# Add a Title to the Plot
+fig.update_layout(title='Standardized Stock Prices')
+
+# Initialize New Columns
+col1, col2 = st.columns(2)
 
 with col1:
-    fig = px.line(prices, x=prices.index, y=prices.columns[2], title=prices.columns[2])
-    fig.update_traces(line_color='lightcoral')
-    fig.update_traces(hovertemplate='Date: %{x}<br>Price: %{y:.2f}')
     st.plotly_chart(fig, use_container_width=True)
+
+###########################################################################
+# Filter the Stock Returns Data by Date
+df_stock_returns = df_stock_returns.filter(pl.col('Date') >= start_date)
+df_stock_returns = df_stock_returns.filter(pl.col('Date') <= end_date)
+
+# Plot the Histogram of Returns colored by Stock
+fig = px.histogram(df_stock_returns.to_pandas(),
+                   x='Return',
+                   color='Stock',
+                   color_discrete_map=color_dict,
+                   marginal='box',
+                   nbins=100,
+                   histnorm='probability density')
+
+fig.update_layout(xaxis_title='Return',
+                    yaxis_title='Density',
+                    showlegend=True)
+
+# Add a Title to the Plot
+fig.update_layout(title='Stock Returns')
 
 with col2:
-    fig = px.line(prices, x=prices.index, y=prices.columns[0], title=prices.columns[0])
-    fig.update_traces(line_color='blue')
-    fig.update_traces(hovertemplate='Date: %{x}<br>Price: %{y:.2f}')
-    st.plotly_chart(fig, use_container_width=True)
-
-
-with col3:
-    fig = px.line(prices, x=prices.index, y=prices.columns[3], title=prices.columns[3])
-    fig.update_traces(line_color='green')
-    fig.update_traces(hovertemplate='Date: %{x}<br>Price: %{y:.2f}')
-    st.plotly_chart(fig, use_container_width=True)
-
-with col4:
-    fig = px.line(prices, x=prices.index, y=prices.columns[1], title=prices.columns[1])
-    fig.update_traces(line_color='red')
-    fig.update_traces(hovertemplate='Date: %{x}<br>Price: %{y:.2f}')
     st.plotly_chart(fig, use_container_width=True)
